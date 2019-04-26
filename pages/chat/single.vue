9ef69515f8e3c85869d7e114184b9313abb098ea
김혜신 <hyeshin2530@gmail.com>
2019-04-26 18:16:04 +0900

    change single chat UI

--- pages/chat/single.vue
+++ pages/chat/single.vue
@@ -1,54 +1,56 @@
 <template>
-  <v-layout align-center row wrap>
-    <v-flex
-      xs12
-      class="display-1"
-    >
-      Single Chat
-    </v-flex>
-    <v-flex
-      xs2
-      class="title"
-    >
-      message
-    </v-flex>
-    <v-flex xs8>
-      <v-text-field v-model="msg" label="메시지를 입력하세요" @keyup.enter="sendMsg" />
-    </v-flex>
-    <v-flex xs2>
-      <v-btn
-        color="success"
-        class="title black--text"
-        @click="sendMsg"
-      >
-        전송
-      </v-btn>
-    </v-flex>
+  <v-container grid-list-xl text-xs-center>
+    <v-layout align-center row wrap>
+      <template>
+        <v-flex
+          xs12
+          class="display-1"
+        >
+          Single Chat
+        </v-flex>
+        <v-flex
+          xs2
+          class="title"
+        >
+          message
+        </v-flex>
+        <v-flex xs8>
+          <v-text-field v-model="msg" label="메시지를 입력하세요" @keyup.enter="sendMsg" />
+        </v-flex>
+        <v-flex xs2>
+          <v-btn
+            color="success"
+            class="title black--text"
+            @click="sendMsg"
+          >
+            전송
+          </v-btn>
+        </v-flex>
+      </template>
 
-    <v-flex
-      xs12
-      class="headline"
-    >
-      TimeLine
-    </v-flex>
-    <v-flex
-      xs12
-      text-xs-left
-    >
-      <v-card>
-        <v-list>
-          <template v-for="(msg, i) in recMsg">
-            <v-list-item :key="i">
-              <v-list-item-content>
-                <v-list-item-title class="body-1" v-text="msg" />
-              </v-list-item-content>
-            </v-list-item>
-            <v-divider v-if="i+1 < recMsg.length" :key="`divider-${i}`" />
-          </template>
-        </v-list>
-      </v-card>
-    </v-flex>
-  </v-layout>
+      <v-flex
+        xs12
+        class="headline"
+      >
+        Chat Room
+      </v-flex>
+
+      <v-flex xs12 text-xs-left>
+        <v-card>
+          <v-list>
+            <template v-for="(msg, i) in recMsg">
+              <v-list-item :key="i">
+                <v-list-item-content>
+                  <v-list-item-title class="body-1" v-text="msg" />
+                </v-list-item-content>
+              </v-list-item>
+              <v-divider v-if="i+1 < recMsg.length" :key="`divider-${i}`" />
+            </template>
+          </v-list>
+        </v-card>
+      </v-flex>
+    </v-layout>
+  </v-container>
 </template>
 
 <script>
@@ -83,7 +85,6 @@ export default {
       }
     }
   }
-
 }
 </script>
 
